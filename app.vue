2b7073c05870735fcdf24261ebbf5e763d710e6e
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-04-24 18:42:18 +0530

    Change app.vue file

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>my name is {{ name }}</h1>
+    <h2>I'm {{ age }} years old.</h2>
+  </div>
+</template>
+<script setup>
+  const name = "Ishan Hansaka";
+  let age = 21;
+</script>
