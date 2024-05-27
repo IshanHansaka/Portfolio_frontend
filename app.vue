c15470b8848416888b7df98f90f36ab4c058e21c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-05-27 12:45:06 +0530

    send POST request to backend from frontend
    create form to input new project data and POST it to data base and GET to front end

--- app.vue
+++ app.vue
@@ -3,10 +3,10 @@
     <h1>My name is {{ name }}</h1>
   </div>
 
-  <div v-if="pending">Loading projects...</div>
-
   <div>
     <h2>Projects</h2>
+    <div v-if="pending">Loading projects...</div>
+    <div v-if="error">Error 404 not found</div>
     <ul>
       <li v-for="project in projects" :key="project.name">
         <h3>{{ project.name }}</h3>
@@ -15,10 +15,10 @@
     </ul>
   </div>
 
-  <div v-if="pending">Loading blogs...</div>
-
   <div>
     <h2>Blogs</h2>
+    <div v-if="pending1">Loading blogs...</div>
+    <div v-if="error1">Error 404 not found</div>
     <ul>
       <li v-for="blog in blogs" :key="blog.title">
         <h3>{{ blog.title }}</h3>
@@ -27,10 +27,57 @@
       </li>
     </ul>
   </div>
+
+  <div>
+    <h1>Create New Project</h1>
+    <form @submit.prevent="createProject"> <!-- prevent refresh after sumbmit -->
+      <div>
+        <label for="name">Project Name : </label>
+        <input type="text" v-model="newProject.name" id="name" required />
+      </div>
+      <div>
+        <label for="description">Project Description : </label>
+        <textarea name="description" v-model="newProject.description" id="description"></textarea>
+      </div>
+      <button type="submit">Create Project</button>
+    </form>
+  </div>
 </template>
 
 <script setup>
-  const name = "Ishan Hansaka";
-  const {data: projects, pending, error, } = useFetch("http://localhost:5000/projects");
-  const {data: blogs, pending1, error1,} = useFetch("http://localhost:5000/blogs");
+  const name = "Ishan Hansaka Silva";
+  const { data: projects, pending, error, } = useFetch("http://localhost:5000/projects");
+  const { data: blogs, pending1, error1, } = useFetch("http://localhost:5000/blogs");
+
+  const newProject = ref({
+    name: "",
+    description: "",
+  });
+
+  const createProject = async () => {
+    console.log(newProject.value);
+    try {
+      const response = await fetch("http://localhost:5000/projects", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newProject.value),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create project");
+      }
+
+      const result = await response.json();
+      projects.value.push(result); //push data to front end
+
+      newProject.value.name = "";//clear the form
+      newProject.value.description = "";
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 </script>
+
+<style></style>
