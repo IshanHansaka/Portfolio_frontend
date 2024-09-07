e634cf2e2fb82146d168c8d399d7988e4608c6ba
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-07 17:09:33 +0530

    feat: add meta data and material icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2024-09-07",
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', "@vesp/nuxt-fontawesome"],
+  app: {
+    head: {
+      title: "Ishan Hansaka Silva",
+      meta: [{ name: "description", content: "This is the official website of Ishan Hansaka Silva, undergraduate of University of Moratuwa, Sri Lanka." }],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
 })
