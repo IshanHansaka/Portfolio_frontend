0b01073532ff88f9fc748f8d056508e4c128b1e0
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-07 17:46:50 +0530

    feat: add nuxt icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2024-09-07",
-  modules: ['@nuxtjs/tailwindcss', "@vesp/nuxt-fontawesome"],
+  modules: ['@nuxtjs/tailwindcss', "@nuxt/icon"],
   app: {
     head: {
       title: "Ishan Hansaka Silva",
