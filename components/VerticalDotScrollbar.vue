e855fb92c388bd9dfa8a70cf33b67fcf816a67e0
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-08 01:19:04 +0530

    chore: change script

--- components/VerticalDotScrollbar.vue
+++ components/VerticalDotScrollbar.vue
@@ -1,10 +1,11 @@
 <template>
-    <nav class="fixed right-5 top-1/2 transform -translate-y-1/2 z-50">
+    <nav class="fixed right-8 top-1/2 transform -translate-y-1/2 z-50">
         <ul class="space-y-2">
-            <li v-for="(section, index) in sections" :key="index" @click="scrollToSection(index)">
+            <li v-for="(section, index) in sections" :key="index">
                 <span
                     :class="['my-3.5 block w-3 h-3 rounded-full cursor-pointer transition-colors duration-300',
-                        { 'bg-gray-900': currentSection === index, 'bg-gray-300 hover:bg-gray-500': currentSection !== index }]">
+                        { 'bg-gray-900': currentSection === index, 'bg-gray-300 hover:bg-gray-500': currentSection !== index }]"
+                    @click="navigateToSection(index)">
                 </span>
             </li>
         </ul>
@@ -13,13 +14,24 @@
 
 <script setup>
 import { ref } from 'vue';
+import { useRouter, useRoute } from '#app';
 
-const sections = ['Home', 'About', 'Projects', 'Articles', 'Contact'];
+const sections = ['', 'about', 'projects', 'articles', 'contact'];
 const currentSection = ref(0);
 
-const scrollToSection = (index) => {
-    currentSection.value = index;
-    document.getElementById(`section-${index}`).scrollIntoView({ behavior: 'smooth' });
+const router = useRouter();
+const route = useRoute();
+
+watch(() => route.path, (newPath) => {
+    const index = sections.indexOf(newPath.replace('/', ''));
+    if (index !== -1) {
+        currentSection.value = index;
+    }
+});
+
+const navigateToSection = (index) => {
+    const path = `/${sections[index]}`;
+    router.push(path);
 };
 </script>
 
