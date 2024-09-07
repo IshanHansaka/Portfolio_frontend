4eca8d2fa434eb1dedbff5e22d11c0e63e3d82a1
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-07 19:18:19 +0530

    feat: add lottie animation component

---
+++ components/LottieAnimation.vue
@@ -0,0 +1,30 @@
+<template>
+    <div ref="lottieContainer"></div>
+</template>
+
+<script>
+import lottie from 'lottie-web';
+
+export default {
+    props: {
+        animationData: {
+            type: Object,
+            required: true
+        }
+    },
+    mounted() {
+        this.loadAnimation();
+    },
+    methods: {
+        loadAnimation() {
+            lottie.loadAnimation({
+                container: this.$refs.lottieContainer,
+                renderer: 'svg',
+                loop: true,
+                autoplay: true,
+                animationData: this.animationData,
+            });
+        }
+    }
+};
+</script>
