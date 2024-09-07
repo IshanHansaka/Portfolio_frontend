54c87093536c42029c8bb85854bd18bc0316ea6f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-08 01:15:32 +0530

    chore: change header and remove routes

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -11,29 +11,10 @@
                                     Ishan Hansaka Silva
                                 </NuxtLink>
                             </div>
-                            <div class="headerNav relative px-7 sm:px-2 md:px-3 xl:px-4 ml-auto">
-                                <ul
-                                    class="text-[1rem] sm:text-[1rem] md:text-[1.1rem] lg:text-[1.2rem] xl:text-[1.3rem] cursor-pointer">
-                                    <li class="inline-block mx-3">
-                                        <NuxtLink to="/">Home</NuxtLink>
-                                    </li>
-                                    <li class="inline-block mx-3">
-                                        <NuxtLink to="/about">About</NuxtLink>
-                                    </li>
-                                    <li class="inline-block mx-3">
-                                        <NuxtLink to="/projects">Projects</NuxtLink>
-                                    </li>
-                                    <li class="inline-block mx-3">
-                                        <NuxtLink to="/articles">Articles</NuxtLink>
-                                    </li>
-                                    <li class="inline-block mx-3">
-                                        <NuxtLink to="/contact">Contact</NuxtLink>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="headerMenu px-1 sm:px-2 md:px-3 xl:px-4">
+                            <div class="headerMenu relative px-7 sm:px-2 md:px-3 xl:px-4 ml-auto">
                                 <i
                                     class="material-icons text-base sm:text-5xl md:text-5xl lg:text-5xl xl:text-5xl cursor-pointer">menu</i>
+
                             </div>
                         </div>
                     </div>
