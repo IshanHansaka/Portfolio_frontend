48e4cf6c05411004b23f5bd2a52381f64b9f08c1
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-08 22:27:15 +0530

    chore: add content

--- components/Aboutsection.vue
+++ components/Aboutsection.vue
@@ -1,14 +1,33 @@
 <template>
-    <div class="min-h-screen px-10 pt-[90px] md:pt-[50px] pb-[100px] flex justify-center items-center">
-        <div>
-            <h2 class="font-bold text-4xl">Sample Section</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab numquam eveniet molestias veniam, inventore
-                sint
-                voluptates nam provident impedit laborum ipsa vel quos amet laboriosam dolorum suscipit! Nihil eos
-                repellendus blanditiis repellat laboriosam veniam quod maxime ab! Ea eveniet doloremque, excepturi
-                totam, et
-                molestias dicta accusamus quibusdam quas sunt inventore!
-            </p>
+    <div class="min-h-screen px-4 pt-16 md:px-10 md:pt-[50px] pb-[100px] flex flex-col items-center justify-center">
+        <div class="flex flex-col md:flex-row justify-center items-start md:items-center gap-5">
+            <!-- Image Section -->
+            <img class="w-full md:w-[100rem] max-w-[200px] md:max-w-[400px] object-cover" alt="Coding"
+                src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Right_Side.gif?raw=true">
+
+            <!-- Text Section -->
+            <div class="text-left w-full md:w-[60%]">
+                <h2 class="font-black mb-4 text-3xl md:text-4xl">About Me</h2>
+                <p class="font-popins text-base md:text-xl mb-4 pr-4">
+                    Hello, I'm Ishan Hansaka Silva, a self-motivated and curious undergraduate at the University of
+                    Moratuwa, pursuing a BSc (Hons) in Information Technology.I am
+                    eager to contribute to the tech industry and explore new opportunities for growth and innovation.
+                </p>
+                <h2 class="font-popins mt-4 mb-1 underline text-xl md:text-2xl">Education</h2>
+                <ul class="list-disc list-inside pl-4">
+                    <li class="mb-2">
+                        <span class="font-semibold">Undergraduate at University of Moratuwa</span>
+                        <br>Bachelor of Science Honours in Information Technology (2023 - 2027)
+                    </li>
+                    <li class="mb-2">
+                        <span class="font-semibold">Ananda Shasthralaya National School</span> <br>Physical Science
+                        stream (2019 - 2022)
+                    </li>
+                    <li class="mb-2">
+                        <span class="font-semibold">Aluthgama Maha Vidyalaya National School</span> (2008 - 2019)
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -16,4 +35,11 @@
 <script setup>
 </script>
 
-<style scoped></style>
+<style scoped>
+/* Ensure responsive design for wider screens */
+@media (min-width: 950px) {
+    .text-left {
+        text-align: left;
+    }
+}
+</style>
