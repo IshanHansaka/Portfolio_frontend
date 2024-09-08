294117d0ac3e65830f63777baba61b4fb75ac5b8
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-08 23:36:14 +0530

    ffeat: add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,10 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'sea1': 'url("https://images.unsplash.com/photo-1615529328331-f8917597711f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")',
-        'sea2': 'url("https://images.unsplash.com/photo-1524704654690-b56c05c78a00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80")',
-        'sea3': 'url("https://images.unsplash.com/photo-1503177847378-d2048487fa46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80")',
+        'stars': 'url("./stars.png")',
+        'planets': 'url("./planets.png")',
+        'sun': 'url("./sun.png")',
+        'mountains': 'url("./mountains.png")'
       },
     },
   },
