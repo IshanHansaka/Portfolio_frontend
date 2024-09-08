300cdb632626adedfc384d464081fdacd7d53857
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-08 21:03:59 +0530

    chore: add content

--- components/HomeSection.vue
+++ components/HomeSection.vue
@@ -1,7 +1,16 @@
 <template>
     <div
-        class="min-h-screen flex justify-center items-center bg-sea1 bg-fixed bg-no-repeat bg-cover bg-center px-10 pt-[90px] md:pt-[50px] pb-[100px]">
-        <h1 class="font-black text-6xl">Parallax</h1>
+        class="min-h-screen flex flex-col md:flex-row items-center justify-center bg-fixed bg-no-repeat bg-cover bg-center px-6 pt-16 pb-16 md:px-10 md:pt-24 md:pb-24">
+        <div class="flex flex-col md:flex-row items-center justify-center gap-6 md:gap-5">
+            <div class="flex flex-col mt-10 items-center md:items-start text-center md:text-left">
+                <h4 class="font-poppins pl-[3px] text-1xl md:text-3xl">Undergraduate at University of Moratuwa</h4>
+                <h1 class="font-black text-4xl md:text-7xl"><span class="">Ishan</span> Hansaka</h1>
+                <button
+                    class="mt-10 px-6 py-2 border-2 border-black bg-black text-white rounded-md hover:bg-white hover:text-black">Download
+                    Resume</button>
+            </div>
+            <img src="../public/ishanpropic.png" alt="Profile Picture" class="w-48 md:w-[25rem] rounded-full mb-10">
+        </div>
     </div>
 </template>
 
