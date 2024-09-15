22106964df7f0127a6070ae7b5b2de1622d301fd
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-15 23:13:06 +0530

    fix: remove path of aboutsection

--- tests/imports.test.ts
+++
@@ -1,6 +0,0 @@
-describe("import vue components", () => {
-    test("normal imports as expected", async () => {
-        const comp = await import("../components/AboutSection.vue")
-        expect(comp).toBeDefined()
-    })
-})
