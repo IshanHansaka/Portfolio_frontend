ce74e815af4445c33676707b06e259be93147277
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-18 20:28:12 +0530

    feat: add layout

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="viewport">
+        <Preloader />
+        <slot />
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    overflow-x: hidden;
+}
+</style>
